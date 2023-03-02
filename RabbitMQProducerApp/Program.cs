2a2b91a6e058da5282d85c08a153fe0cfecd69bf
mahfuz shazol <6895mahfuz@gmail.com>
2023-03-03 00:26:00 +0600

    created Rabbit MQ basic Producer

---
+++ RabbitMQProducerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json.Serialization;
+
+public class Program
+{
+
+
+    public static class Config
+    {
+        public static string RABBIT_MQ_URI = "amqp://guest:guest@localhost:5672";
+        public static string QUEUE_NAME = "demo-queue";
+    }
+
+    public static void Main(string[] args)
+    {
+
+
+
+        ConnectionFactory factory = new ConnectionFactory { Uri = new Uri(Config.RABBIT_MQ_URI) };
+        using IConnection connection = factory.CreateConnection();
+        using IModel channel = connection.CreateModel();
+
+        channel.QueueDeclare(Config.QUEUE_NAME,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+        var message = new { Name = "TestName", Message = "Test Message" };
+        byte[]? body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        channel.BasicPublish("", Config.QUEUE_NAME, null, body);
+
+    }
+}
