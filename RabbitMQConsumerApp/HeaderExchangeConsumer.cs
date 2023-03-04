f3a72424aff3d96e1e3a0d357aa494e7bd306397
mahfuz shazol <6895mahfuz@gmail.com>
2023-03-05 01:14:50 +0600

    Header Exchange in RabbitMQ

---
+++ RabbitMQConsumerApp/HeaderExchangeConsumer.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using StatisConfig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQConsumerApp
+{
+    public static class HeadeExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("demo-header-exchange", ExchangeType.Headers);
+            channel.QueueDeclare(Config.QUEUE_NAME,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+            Dictionary<string, object> header = new Dictionary<string, object> { { "account", "new" } };
+            channel.QueueBind(Config.QUEUE_NAME, "demo-header-exchange", string.Empty,header);
+
+            channel.BasicQos(0, 10, false);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                byte[] body = e.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume(Config.QUEUE_NAME, true, consumer);
+
+             Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQProducerApp/HeaderExchangePublisher.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQProducerApp
+{
+    public static class HeaderExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+
+            Dictionary<string, object> ttl = new Dictionary<string, object>{
+                { "x-message-ttl",30000}
+            };
+
+            channel.ExchangeDeclare("demo-header-exchange", ExchangeType.Headers, arguments: ttl);
+
+            int count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "TestName", Message = "Test Message" };
+                byte[]? body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject($"message {count}"));
+
+
+                var properties = channel.CreateBasicProperties();
+                properties.Headers = new Dictionary<string, object> { { "account", "new" } };
+
+                channel.BasicPublish("demo-header-exchange", string.Empty, properties, body);
+                count++;
+
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
